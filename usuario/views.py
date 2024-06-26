4d3e38e3af8e3b125b9f8ff754f0f883332f8f91
Samantaks <saksamantha@gmail.com>
2024-06-26 15:38:59 -0300

    Resolvi o css.

--- usuario/views.py
+++ usuario/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def loginpage(request):
-    return render(request, "login-page.html")
+    return render(request, "usuario/login-page.html")
 
 
 def cadastropage(request):
-    return render(request, "cadastro-page.html")
+    return render(request, "usuario/cadastro-page.html")
