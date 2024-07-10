3472c48babde308567c64cf015d4083ee0ce0f5d
Samantaks <saksamantha@gmail.com>
2024-07-10 12:00:53 -0300

    Começo do app app

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='login-page')
+def dashboardpage(request):
+    return render(request, "app/dashboard.html")
+
+
+@login_required(login_url='login-page')
+def estoquepage(request):
+    return render(request, "app/estoque.html")
+
+
+@login_required(login_url='login-page')
+def localizacaopage(request):
+    return render(request, "app/estoque.html")
+
+
+@login_required(login_url='login-page')
+def manutencaopage(request):
+    return render(request, "app/manutencao.html")
