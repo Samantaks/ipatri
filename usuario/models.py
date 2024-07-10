92ac55154cfbdf74d5407eeb26b32425d076c40a
Samantaks <saksamantha@gmail.com>
2024-07-10 16:36:53 -0300

    Models Usuário

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Setor(models.Model):
+    id_setor = models.AutoField(primary_key=True)
+    setor_abrev = models.CharField(max_length=250, db_collation='utf8mb3_general_ci')
+    orgao_full = models.CharField(max_length=250, db_collation='utf8mb3_general_ci')
+    setor_full = models.CharField(max_length=250, db_collation='utf8mb3_general_ci')
+    orgao_abrev = models.CharField(max_length=250, db_collation='utf8mb3_general_ci')
+
+    class Meta:
+        managed = False
+        db_table = 'setor'
+
+
+class Usuario(models.Model):
+    idusuario = models.AutoField(primary_key=True)
+    nomeusuario = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=100)
+    email = models.CharField(max_length=150)
+    senha = models.CharField(max_length=50)
+    role = models.CharField(max_length=1)
+    cpf = models.IntegerField(db_column='CPF')  # Field name made lowercase.
+    setor_id_setor = models.ForeignKey(Setor, models.DO_NOTHING, db_column='setor_id_setor')
+
+    class Meta:
+        managed = False
+        db_table = 'usuario'
--- usuario/views.py
+++ usuario/views.py
@@ -2,7 +2,6 @@ from usuario.forms import LoginForm, CadastroForm
 from django.shortcuts import render
 
 
-# Create your views here.
 def loginpage(request):
     if request.method == "GET":
         form = LoginForm
@@ -12,12 +11,11 @@ def loginpage(request):
         return render(request, 'usuario/login-page.html', context=context)
     else:
         form = LoginForm(request.POST)
-        print(form.is_valid())
-        form = LoginForm(request.POST)
         context = {
             'form': form,
         }
-        return render(request, 'usuario/login-page.html', context=context)
+
+        return render(request, "usuario/login-page.html", context=context)
 
 
 def cadastropage(request):
@@ -29,9 +27,9 @@ def cadastropage(request):
         return render(request, 'usuario/cadastro-page.html', context=context)
     else:
         form = CadastroForm(request.POST)
-        print(form.is_valid())
-        form = CadastroForm(request.POST)
         context = {
             'form': form,
         }
+
+
         return render(request, 'usuario/cadastro-page.html', context=context)
