9ac8d81d857b76e7dc88ff47b17a697d83fc0ee3
Samantaks <saksamantha@gmail.com>
2024-07-22 11:30:06 -0300

    Models de Patrimônio

--- patrimonio/models.py
+++ patrimonio/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from usuario.models import Setor
+
+
+class Contacontabil(models.Model):
+    idcontacontabil = models.AutoField(primary_key=True)
+    tipocontacontabil = models.CharField(max_length=50)
+    durabilidade = models.CharField(max_length=11)
+    item_idpatrimonio = models.ForeignKey('Item', models.DO_NOTHING, db_column='item_idpatrimonio')
+
+    class Meta:
+        managed = False
+        db_table = 'contacontabil'
+
+
+class Depreciacao(models.Model):
+    iddepreciacao = models.AutoField(primary_key=True)
+    tipodepreciacao = models.CharField(max_length=100)
+    datacomprafk = models.DateField(blank=True, null=True)
+    datamanutencaofk = models.DateField(blank=True, null=True)
+    valordepreciado = models.DecimalField(max_digits=9, decimal_places=5)
+
+    class Meta:
+        managed = False
+        db_table = 'depreciacao'
+
 
 # Create your models here.
+class Item(models.Model):
+    idpatrimonio = models.AutoField(primary_key=True)
+    tombo = models.IntegerField()
+    marca = models.CharField(max_length=250)
+    descricao = models.TextField(blank=True, null=True)
+    datacompra = models.DateField()
+    valorcompra = models.DecimalField(max_digits=20, decimal_places=5)
+    notafiscal = models.IntegerField()
+    depreciacao_iddepreciacao1 = models.ForeignKey(Depreciacao,
+                                                   models.DO_NOTHING, db_column='depreciacao_iddepreciacao1')
+    setor_id_setor = models.ForeignKey(Setor, models.DO_NOTHING, db_column='setor_id_setor')
+
+    class Meta:
+        managed = False
+        db_table = 'item'
--- usuario/admin.py
+++ usuario/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Usuario ,Setor
+from .models import Usuario, Setor
 
 # Register your models here.
 admin.site.register(Usuario)
